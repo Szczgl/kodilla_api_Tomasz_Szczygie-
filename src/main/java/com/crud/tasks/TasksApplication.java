4d34e67c3698f67d0a07faf5b7c6c548f019f7ba
Tomasz Szczygiel <szczgl@wp.pl>
2024-02-27 20:18:56 +0100

    18.2 Added kodilla Api

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,15 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-
-		TaskDto taskDto = new TaskDto(1L, "Test title", "I want to be coder!");
-
-		Long id = taskDto.getId();
-		String title = taskDto.getTitle();
-		String content = taskDto.getContent();
-
-		System.out.println(id + " " + title + " " + content);
-
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
