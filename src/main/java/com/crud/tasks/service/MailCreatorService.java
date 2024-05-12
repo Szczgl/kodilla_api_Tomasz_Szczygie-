6046386808f610acf5eb2bcfd0211f2f50444c22
Tomasz Szczygiel <szczgl@wp.pl>
2024-05-12 23:01:22 +0200

    thymeleaf 30.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.*;
+
 @Service
 public class MailCreatorService {
 
@@ -18,6 +20,11 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://szczgl.github.io");
@@ -25,7 +32,25 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye", "See you on our website");
         context.setVariable("company", adminConfig.getCompany());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("created-trello-card-mail", context);
+    }
+
+    public String buildNumberOfTasksEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://szczgl.github.io");
+        context.setVariable("button", "Tasks");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "See you on our website");
+        context.setVariable("company", adminConfig.getCompany());
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        return templateEngine.process("number-of-tasks", context);
     }
 
 }
